13ef120bd1849abbf0f254cc6e5b2993616fed7d
Jensone <hello@jensone.com>
2024-01-19 15:54:43 +0100

    📝 ajout(RoomController.php) : ajouter un contrôleur pour gérer les salles 🔀 fusion(RoomController.php) : fusionner les modifications du fichier RoomController.php

---
+++ src/Controller/RoomController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RoomRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/r')] // prefix all room routes with /r
+class RoomController extends AbstractController
+{
+    #[Route('/', name: 'app_room')]
+    public function index(
+        RoomRepository $roomRepository
+    ): Response
+    {
+        return $this->render('room/index.html.twig', [
+            'rooms' => $roomRepository->findAll()
+        ]);
+    }
+}
